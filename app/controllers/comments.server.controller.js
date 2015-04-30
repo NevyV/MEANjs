ac0108777dd0356adaffa3f4c0528da352dd6ad0
bpayne915 <bpayne915@yahoo.com>
2015-04-29 20:40:39 -0400

    sockets off comments

--- app/controllers/comments.server.controller.js
+++ app/controllers/comments.server.controller.js
@@ -21,8 +21,7 @@ exports.create = function(req, res) {
 				message: errorHandler.getErrorMessage(err)
 			});
 		} else {
-			var socketio = req.app.get('socketio'); //makes a socket instance
-			socketio.emit('article.created', article); //sends the socket event to all current users
+			
 			res.jsonp(comment);
 		}
 	});
