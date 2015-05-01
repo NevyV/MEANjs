d9ca059664f02ca95ccb4928ab4e4022b78dc7fe
bpayne915 <bpayne915@yahoo.com>
2015-05-01 11:53:10 -0400

    Remove profile from header

--- public/modules/profiles/config/profiles.client.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Configuring the Articles module
-angular.module('profiles').run(['Menus',
-	function(Menus) {
-		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Profile', 'profiles', '/profiles(/create)?');
-	}
-]);
